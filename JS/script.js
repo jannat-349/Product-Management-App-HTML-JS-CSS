fd0792e005e1684d243bfbf44997e094461f5f72
jannat28-349 <jucse2.349@gmail.com>
2023-01-21 12:45:37 +0600

    Added nothing to show and final input taking done

--- JS/script.js
+++ JS/script.js
@@ -6,16 +6,26 @@ function getInput() {
   };
 }
 
+function addTableHead(tblHead) {
+  tblHead.innerHTML = `
+    <th>PRODUCT ID</th>
+    <th>PRODUCT NAME</th>
+    <th>PRODUCT PRICE</th>
+    <th>OPTION</th>`;
+}
+
 function addProduct() {
+  const tblHead = document.getElementById("tbl-head");
+  addTableHead(tblHead);
   let product = getInput();
   const table = document.getElementById("tbl");
   const e1 = document.createElement("tr");
   e1.innerHTML = `<td>${product.pid}</td>
-  <td>${product.pname}</td>
-  <td>${product.price}</td>
-  <td>
-    <button>EDIT</button>
-    <button>DELETE</button>
-  </td>`;
+    <td>${product.pname}</td>
+    <td>${product.price}</td>
+    <td>
+      <button>EDIT</button>
+      <button>DELETE</button>
+    </td>`;
   table.appendChild(e1);
 }
