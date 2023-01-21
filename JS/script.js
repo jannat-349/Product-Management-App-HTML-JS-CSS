675dac0164dee4d2b3ddaf9bdc3df164382b3f0a
jannat28-349 <jucse2.349@gmail.com>
2023-01-21 22:28:58 +0600

    Merged all the validation function in one function

--- JS/script.js
+++ JS/script.js
@@ -3,62 +3,65 @@ let productName;
 let productPrice;
 let count = 0;
 function isEmpty() {
-  return (productId && productName && productPrice)
-    ? false
-    : true;
+  return productId && productName && productPrice ? false : true;
 }
 const pidList = [];
 function isIdDupicate() {
   return pidList.includes(productId);
 }
 function isLongerPName() {
-  return (productName.length > 60)? true : false;
+  return productName.length > 60 ? true : false;
 }
 function isPriceNegative() {
-  return (productPrice < 0)? true : false;
+  return productPrice < 0 ? true : false;
 }
 function isPriceValid() {
-  return (productPrice <= 100000.00) ? true : false;
+  return productPrice <= 100000.0 ? true : false;
 }
 function addInputMsg() {
   const msg = document.getElementById("msg");
   msg.innerText = `Nice to have the product added`;
-  setTimeout(function() {
+  setTimeout(function () {
     msg.innerText = ``;
-  }, 3000); 
+  }, 3000);
+}
+function validateInput() {
+  if (isEmpty()) {
+    alert("Fields can not be empty!");
+    return false;
+  }
+  if (isIdDupicate(productId)) {
+    alert("Product ID must be unique!");
+    return false;
+  }
+  if (isLongerPName()) {
+    alert("Product name can not be longer than 60 characters!");
+    return false;
+  }
+  if (isPriceNegative()) {
+    alert("Product price can not be negative");
+    return false;
+  }
+  if (!isPriceValid()) {
+    alert("Price can not be more than 100000.00");
+    return false;
+  }
+  return true;
 }
 function getInput() {
   productId = document.getElementById("pid").value;
   productName = document.getElementById("pname").value.trim();
   productPrice = document.getElementById("price").value;
-  if(isEmpty()) {
-    alert("Fields can not be empty!");
-    return;
+  if (validateInput()) {
+    count++;
+    pidList.push(productId);
+    addInputMsg();
+    return {
+      pid: productId,
+      pname: productName,
+      price: productPrice,
+    };
   }
-  if(isIdDupicate(productId)) {
-    alert("Product ID must be unique!")
-    return;
-  }
-  if(isLongerPName()) {
-    alert("Product name can not be longer than 60 characters!");
-    return;
-  }
-  if(isPriceNegative()) {
-    alert("Product price can not be negative");
-    return;
-  }
-  if(!isPriceValid()) {
-    alert("Price can not be more than 100000.00");
-    return;
-  }
-  count++;
-  pidList.push(productId);
-  addInputMsg();
-  return {
-    pid: productId,
-    pname: productName,
-    price: productPrice,
-  };
 }
 
 function addTableHead(tblHead) {
@@ -68,7 +71,6 @@ function addTableHead(tblHead) {
     <th>PRODUCT PRICE</th>
     <th>OPTION</th>`;
 }
-
 
 function addProduct() {
   let product = getInput();
