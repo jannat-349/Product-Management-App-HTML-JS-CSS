193bb98610e5fc82c7d3909c4aea4f867a228c7d
jannat28-349 <jucse2.349@gmail.com>
2023-01-27 23:42:25 +0600

    Added Product class and done with add delete and validation

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,7 @@
-let productId;
-let productName;
-let productPrice;
 let count = 0;
 let editMode = false;
-function isEmpty() {
-  return productId && productName && productPrice ? false : true;
-}
-const pidList = [];
-function isIdDupicate() {
-  return pidList.includes(productId);
-}
-function isLongerPName() {
-  return productName.length > 60 ? true : false;
-}
-function isPriceNegative() {
-  return productPrice < 0 ? true : false;
-}
-function isPriceValid() {
-  return productPrice <= 100000.0 ? true : false;
-}
+let pidList = [];
+let productList = [];
 function addInputMsg() {
   const msg = document.getElementById("msg");
   msg.innerText = `Nice to have the product added`;
@@ -26,45 +9,66 @@ function addInputMsg() {
     msg.innerText = ``;
   }, 3000);
 }
-function validateInput() {
-  if (isEmpty()) {
-    alert("Fields can not be empty!");
-    return false;
+
+class Product {
+  constructor(pid, pname, price) {
+    this.productId = pid;
+    this.productName = pname;
+    this.productPrice = price;  
   }
-  if(!editMode){
-    if (isIdDupicate(productId)) {
-      alert("Product ID must be unique!");
+  isEmpty() {
+    return Boolean(!(this.productId && this.productName && this.productPrice));
+  }
+  isIdDupicate() {
+    return pidList.includes(this.productId);
+  }
+  isLongerPName() {
+    return this.productName.length > 60 ? true : false;
+  }
+  isPriceNegative() {
+    return this.productPrice < 0 ? true : false;
+  }
+  isPriceValid() {
+    return this.productPrice <= 100000.0 ? true : false;
+  }
+  validateProduct() {
+    if (this.isEmpty()) {
+      alert("Fields can not be empty!");
       return false;
     }
+    if(!editMode){
+      if (this.isIdDupicate()) {
+        alert("Product ID must be unique!");
+        return false;
+      }
+    }
+    if (this.isLongerPName()) {
+      alert("Product name can not be longer than 60 characters!");
+      return false;
+    }
+    if (this.isPriceNegative()) {
+      alert("Product price can not be negative");
+      return false;
+    }
+    if (!this.isPriceValid()) {
+      alert("Price can not be more than 100000.00");
+      return false;
+    }
+    return true;
   }
-  if (isLongerPName()) {
-    alert("Product name can not be longer than 60 characters!");
-    return false;
-  }
-  if (isPriceNegative()) {
-    alert("Product price can not be negative");
-    return false;
-  }
-  if (!isPriceValid()) {
-    alert("Price can not be more than 100000.00");
-    return false;
-  }
-  return true;
-}
 
+}
 function getInput() {
-  productId = document.getElementById("pid").value;
-  productName = document.getElementById("pname").value.trim();
-  productPrice = document.getElementById("price").value;
-  if (validateInput()) {
+  let pid = document.getElementById("pid").value;
+  let pname = document.getElementById("pname").value.trim();
+  let price = document.getElementById("price").value;
+  let product = new Product(pid, pname, price);
+  if (product.validateProduct()) {
+    productList.push(product);
     count++;
-    pidList.push(productId);
+    pidList.push(product.productId);
     addInputMsg();
-    return {
-      pid: productId,
-      pname: productName,
-      price: productPrice,
-    };
+    return product;
   }
 }
 
@@ -121,63 +125,64 @@ function clearInput() {
   <button id="add-product-btn" onclick="addProduct()">ADD</button>
 </div>`;
 }
-function updateProduct(pp) {
-  editMode = true;
-  let row = document.getElementById(pp);
-  let pId = document.getElementById("pid");
-  let pName = document.getElementById("pname");
-  let pPrice = document.getElementById("price");
-  productId = pId.value;
-  productName = pName.value;
-  productPrice = pPrice.value;
-  if (validateInput()) {
-    let add = document.getElementById("add");
-    add.innerHTML = `<button id="add-product-btn" onclick="addProduct()">ADD</button>`;
-    row.innerHTML = `<td>${productId}</td>
-      <td>${productName}</td>
-      <td>${productPrice}</td>
-      <td>
-        <button class = "edit-btn" onclick = "editProduct(${row.getAttribute(
-          "id"
-        )})">EDIT</button>
-        <button class = "delete-btn" onclick = "confirmation(${row.getAttribute(
-          "id"
-        )})">DELETE</button>
-      </td>`;
-    pId.disabled = false;
-    clearInput();
-  }
-}
+// function updateProduct(pp) {
+//   editMode = true;
+//   let row = document.getElementById(pp);
+//   let pId = document.getElementById("pid");
+//   let pName = document.getElementById("pname");
+//   let pPrice = document.getElementById("price");
+//   productId = pId.value;
+//   productName = pName.value;
+//   productPrice = pPrice.value;
+//   if (validateInput()) {
+//     let add = document.getElementById("add");
+//     add.innerHTML = `<button id="add-product-btn" onclick="addProduct()">ADD</button>`;
+//     row.innerHTML = `<td>${productId}</td>
+//       <td>${productName}</td>
+//       <td>${productPrice}</td>
+//       <td>
+//         <button class = "edit-btn" onclick = "editProduct(${row.getAttribute(
+//           "id"
+//         )})">EDIT</button>
+//         <button class = "delete-btn" onclick = "confirmation(${row.getAttribute(
+//           "id"
+//         )})">DELETE</button>
+//       </td>`;
+//     pId.disabled = false;
+//     clearInput();
+//   }
+// }
 
-function editProduct(pp) {
-  let row = document.getElementById(pp);
-  let cells = row.getElementsByTagName("td");
-  // alert(cells[0].innerText);
-  // alert(cells[1].innerText);
-  // alert(cells[2].innerText);
-  let pID = document.getElementById("pid");
-  pID.value = cells[0].innerText;
-  pID.disabled = true;
-  let pName = document.getElementById("pname");
-  pName.value = cells[1].innerText;
-  let pPrice = document.getElementById("price");
-  pPrice.value = cells[2].innerText;
-  let update = document.getElementById("add");
-  update.innerHTML = `<button id="update-product-btn" onclick="updateProduct(${pp})">UPDATE</button>`;
-}
+// function editProduct(pp) {
+//   let row = document.getElementById(pp);
+//   let cells = row.getElementsByTagName("td");
+//   // alert(cells[0].innerText);
+//   // alert(cells[1].innerText);
+//   // alert(cells[2].innerText);
+//   let pID = document.getElementById("pid");
+//   pID.value = cells[0].innerText;
+//   pID.disabled = true;
+//   let pName = document.getElementById("pname");
+//   pName.value = cells[1].innerText;
+//   let pPrice = document.getElementById("price");
+//   pPrice.value = cells[2].innerText;
+//   let update = document.getElementById("add");
+//   update.innerHTML = `<button id="update-product-btn" onclick="updateProduct(${pp})">UPDATE</button>`;
+// }
 
 function addProduct() {
   editMode = false;
   let product = getInput();
+
   if (count > 0) {
     const tblHead = document.getElementById("tbl-head");
     addTableHead(tblHead);
     const table = document.getElementsByTagName("tbody")[0];
     const e1 = document.createElement("tr");
-    e1.setAttribute("id", product.pid);
-    e1.innerHTML = `<td>${product.pid}</td>
-    <td>${product.pname}</td>
-    <td>${product.price}</td>
+    e1.setAttribute("id", product.productId);
+    e1.innerHTML = `<td>${product.productId}</td>
+    <td>${product.productName}</td>
+    <td>${product.productPrice}</td>
     <td>
       <button class = "edit-btn" onclick = "editProduct(${e1.getAttribute(
         "id"
